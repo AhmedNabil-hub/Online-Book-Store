dfdc13f0bef75ee33b8762cccf24ba8a2dffe54d
AhmedNabil-hub <57273922+AhmedNabil-hub@users.noreply.github.com>
2021-05-28 22:39:56 +0200

    Update user_form.php

--- user_form.php
+++ user_form.php
@@ -162,17 +162,25 @@
             <button class="btn btn-primary btn-user btn-block" type="submit">Save</button>
 
           </form>
+          <?php
+            if (!empty($formErrors)){
+              foreach ($formErrors as $error) {
+                echo "<div class='row' role='alert'>";
+                  echo "<div class='col-sm-2'></div>";
+                  echo "<div class='col-sm-10 mt-3'>";
+                    echo "<div class='alert-icon' style='display:inline-block;margin-right:10px'>";
+                      echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-x-circle-fill' viewBox='0 0 16 16'>";
+                        echo "<path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/>";
+                      echo "</svg>";
+                    echo "</div>";
+                    echo "<span>". $error ."</span>";
+                  echo "</div>";
+                echo "</div>";
+              }
+            }
+          ?>
         </div>
       </div>
-      <?php
-                if (!empty($formErrors)){
-                  foreach ($formErrors as $error) {
-                    echo "<div class='alert alert-danger text-center'>";
-                      echo $error;
-                    echo "</div>";
-                  }
-                }
-              ?>
     </div>
   </div>
 </div>
